30de48b8ca80f86028175e020a50884523a28907
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-04-18 09:36:23 +0530

    First commit

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,11 @@
+import './topbar.css'
+
+export default function Topbar()
+{
+    return (
+        <div className='topbarContainer'>
+            .topbarLeft
+
+        </div>
+    )
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import Person2Icon from '@mui/icons-material/Person2';
+
+export default function Home()
+{
+    return(
+        <div>
+            this is home
+        </div>
+    )
+}

43171f6cc1bdc21c737fc242e96fa6bc247ff033
sulakshigunarathne <gunarathneawsm.20@uom.lk>
2024-04-18 13:43:08 +0530

    home pabge

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Topbar from './components/topbar/Topbar';
+import Sidebar from './components/sidebar/Sidebar';
+import Rightbar from './components/rightbar/Rightbar';
+import Feed from './components/feed/Feed';
+
+
+function App() {
+  return (
+    <div >
+      <Topbar/>
+      {/* <Sidebar/>
+      <Feed/>
+      <Rightbar/> */}
+    
+    </div>
+  );
+}
+
+export default App;
